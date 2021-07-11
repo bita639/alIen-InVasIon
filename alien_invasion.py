99ed950dfa5d88377195b7b275bbb6ae4e768935
bita639 <01625326156dj@gmail.com>
2021-07-12 03:04:28 +0600

    system successfully runed and ship icon set to centered of screen and screen size risized.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     #inititalize pygame, settings and screen object.
@@ -8,6 +9,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    #make a ship
+    ship = Ship(screen)
 
     #set the background color.
     bg_color = (230, 230, 230)
@@ -23,6 +27,7 @@ def run_game():
 
         #Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         
         #make the most recently drawn screen visible.
         pygame.display.flip()
